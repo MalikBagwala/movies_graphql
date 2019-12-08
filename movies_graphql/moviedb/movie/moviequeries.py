202a693423341e21ebca18fb8fa93e03760ea2fc
Malik Bagwala <malikeditz@gmail.com>
2019-12-08 12:33:35 +0530

    added ability to query a single movie

--- movies_graphql/moviedb/movie/moviequeries.py
+++ movies_graphql/moviedb/movie/moviequeries.py
@@ -6,6 +6,7 @@ from movies_graphql.moviedb.movie.movietypes import MovieType
 
 class MovieQuery(object):
     all_movies = graphene.List(MovieType, search=graphene.String())
+    movie = graphene.Field(MovieType, id=graphene.ID())
 
     def resolve_all_movies(self, info, **kwargs):
         search = kwargs.get("search")
@@ -13,3 +14,11 @@ class MovieQuery(object):
             filter = (Q(title__icontains=search))
             return Movie.objects.filter(filter)
         return Movie.objects.order_by("-created_at")
+
+    def resolve_movie(self, info, **kwargs):
+        id = kwargs.get("id")
+
+        if id is not None:
+            return Movie.objects.get(pk=id)
+
+        return None
