c32d900aad19172f87a4a87413726db1971e15cd
Malik Bagwala <malikeditz@gmail.com>
2019-12-09 19:16:43 +0530

    fixed warnings

--- movies_graphql/moviedb/actor/actorqueries.py
+++ movies_graphql/moviedb/actor/actorqueries.py
@@ -10,15 +10,14 @@ class ActorQuery(object):
     all_actors = graphene.List(ActorType, search=graphene.String())
     actor = graphene.Field(ActorType, id=graphene.ID())
 
-    def resolve_all_actors(self, info, **kwargs):
+    def resolve_all_actors(self, _, **kwargs):
         search = kwargs.get("search")
         if search:
             filter = (Q(name__icontains=search))
             return Actor.objects.filter(filter)
         return Actor.objects.all()
 
-    def resolve_actor(self, info, **kwargs):
+    def resolve_actor(self, _, **kwargs):
         id = kwargs.get("id")
 
         return Actor.objects.get(pk=id)
-    pass
