f899e2138f80cd6283e0bb94441632e23775934e
MalikBagwala <malikeditz@gmail.com>
2019-12-21 11:26:12 +0530

    added env

--- movies_graphql/settings.py
+++ movies_graphql/settings.py
@@ -15,15 +15,15 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+VT_ENV = os.environ.get("VT_ENV", "development")
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False if VT_ENV == "production" else True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '&^2c^wg71v@te@hl*5^_-xawgw3ib+nnt8c#h%idm2p_zd912$'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "django-graphql.herokuapp.com"]
 
@@ -137,4 +137,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(
+    BASE_DIR, 'static') if VT_ENV == "production" else None
